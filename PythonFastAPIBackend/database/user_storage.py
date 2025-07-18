a03b4e5828ecf0823baadb28477b24d3d2c14e25
anthonytast <anthonyftast@gmail.com>
2025-07-18 16:47:59 -0400

    added pw hashing and cleaned up endpoints. still need to adjust controllers in js

--- PythonFastAPIBackend/database/user_storage.py
+++ PythonFastAPIBackend/database/user_storage.py
@@ -1,3 +1,5 @@
+from passlib.context import CryptContext
+
 class UserDB():
     username: str
     first_name: str
@@ -12,19 +14,21 @@ class UserDB():
         self.phone_number = phone_number
         self.password = password
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 users = {
     "anthonytast": UserDB(
         username="anthonytast",
         first_name="Anthony",
         last_name="Tast",
         phone_number="631-925-7508",
-        password="SuperSecretPW"
+        password=bcrypt_context.hash("SuperSecretPW")
     ),
     "testuser": UserDB(
         username="testuser",
         first_name="Dev",
         last_name="Tester",
         phone_number="1-800",
-        password="test"
+        password=bcrypt_context.hash("test")
     )
 }
